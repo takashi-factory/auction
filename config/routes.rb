202cbf5484143be10613bfe7ec7481c10fb40647
takashi <nandeyanen08@gmail.com>
2020-07-08 20:08:04 +0900

    詳細画面を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # get '/items' => 'items#index'
+  # post '/items' => 'items#create'
+  # get '/items/new' => 'items#new'
+  # get '/items/:id' => 'items#show'
+  
+  resources :items
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
